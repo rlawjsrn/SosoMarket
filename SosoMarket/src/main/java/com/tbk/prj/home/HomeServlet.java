74078d4f77bb07520a86f3913732ba06549c50f0
Lilpalled <126761148+Lilpalled@users.noreply.github.com>
2023-10-24 17:23:41 +0900

    home.jsp + homeServlet 생성

---
+++ SosoMarket/src/main/java/com/tbk/prj/home/HomeServlet.java
@@ -0,0 +1,33 @@
+package com.tbk.prj.home;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/Home.do")
+public class HomeServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	
+	public HomeServlet() {
+		super();
+	}
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html;charset=UTF-8");
+		response.setCharacterEncoding("UTF-8");
+		
+		RequestDispatcher dispatcher = request.getRequestDispatcher("home.jsp");
+		dispatcher.forward(request, response);
+	}
+
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
