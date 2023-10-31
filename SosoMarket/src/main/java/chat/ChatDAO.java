cc6eb7245f345254b0004f7ad3bed69ffc470697
soconzan <54516219+soconzan@users.noreply.github.com>
2023-10-31 17:41:04 +0900

    231031머지

---
+++ SosoMarket/src/main/java/chat/ChatDAO.java
@@ -0,0 +1,13 @@
+package chat;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+import common.DAO;
+
+public class ChatDAO extends DAO {
+	
+	private PreparedStatement psmt;
+	private ResultSet rs;
+	
+}
