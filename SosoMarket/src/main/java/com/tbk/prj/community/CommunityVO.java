cf0c72425fb05bfe79d441a93b97d3e6a37c3513
odinaazizova <odinaazizova@172.26.3.84>
2023-11-06 18:19:30 +0900

    20231106 dina commit

--- SosoMarket/src/main/java/com/tbk/prj/community/CommunityVO.java
+++ SosoMarket/src/main/java/com/tbk/prj/community/CommunityVO.java
@@ -1,5 +1,74 @@
 package com.tbk.prj.community;
 
-public class CommunityVO {
 
+import java.util.Date; 
+
+
+public class CommunityVO {
+    private String postId;
+    private String postTitle;
+    private String member_id; 
+    private String postDetail;
+    private int postViews;
+    private Date generationDate;
+
+
+    public String getPostId() {
+        return postId;
+    }
+
+
+    public void setPostId(String postId) {
+        this.postId = postId;
+    }
+
+
+    public String getPostTitle() {
+        return postTitle;
+    }
+
+
+    public void setPostTitle(String postTitle) {
+        this.postTitle = postTitle;
+    }
+
+
+    public String getMemberId() {
+        return member_id;
+    }
+
+
+    public void setMemberId(String member_id) {
+        this.member_id = member_id;
+    }
+
+
+    public String getPostDetail() {
+        return postDetail;
+    }
+
+
+    public void setPostDetail(String postDetail) {
+        this.postDetail = postDetail;
+    }
+
+
+    public int getPostViews() {
+        return postViews;
+    }
+
+
+    public void setPostViews(int postViews) {
+        this.postViews = postViews;
+    }
+
+
+    public Date getGenerationDate() {
+        return generationDate;
+    }
+
+
+    public void setGenerationDate(Date generationDate) {
+        this.generationDate = generationDate;
+    }
 }
