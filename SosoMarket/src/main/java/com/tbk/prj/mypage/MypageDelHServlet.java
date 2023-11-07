9a04d439238fb94f40f0d9849d031cb2ecc81bc7
aekgmla77 <74331978+aekgmla77@users.noreply.github.com>
2023-11-07 10:48:53 +0900

    수정완

--- SosoMarket/src/main/java/com/tbk/prj/mypage/MypageDelHServlet.java
+++ SosoMarket/src/main/java/com/tbk/prj/mypage/MypageDelHServlet.java
@@ -1,54 +1,61 @@
 package com.tbk.prj.mypage;
 
-import jakarta.servlet.RequestDispatcher;
-import jakarta.servlet.ServletException;
-import jakarta.servlet.annotation.WebServlet;
-import jakarta.servlet.http.HttpServlet;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.ArrayList;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 /**
  * Servlet implementation class MypageLikeList
  */
-@WebServlet("/MypageLikeList.do")
+@WebServlet("/MypageDelH.do")
 public class MypageDelHServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public MypageDelHServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#HttpServlet()
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	public MypageDelHServlet() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		request.setCharacterEncoding("utf-8");
 		MypageLikeDAO dao = new MypageLikeDAO();
 		MypageLikeVO vo = new MypageLikeVO();
 		ArrayList<MypageLikeVO> list = new ArrayList<MypageLikeVO>();
-		
+
 		// dao 페이지에서 전체목록
-		list = dao.selectList();
+		list = dao.buyList();
 		System.out.println(list);
 		request.setAttribute("list", list);
 		// 목록을 보여줄 페이지
-		String viewPage = "mypage/myLikeList.jsp";
-		// RequestDispatcher는 클라이언트로부터 최초에 들어온 요청을 JSP/Servlet 내에서 원하는 자원으로 요청을 넘기는 역할 수행
+		String viewPage = "mypage/myBuyList.jsp";
+		// RequestDispatcher는 클라이언트로부터 최초에 들어온 요청을 JSP/Servlet 내에서 원하는 자원으로 요청을 넘기는 역할
+		// 수행
 		RequestDispatcher dispatcher = request.getRequestDispatcher(viewPage);
 		dispatcher.forward(request, response);
+
 	}
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		doGet(request, response);
 	}
